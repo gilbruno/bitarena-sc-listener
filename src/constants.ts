13e8fc4486f62d59af001e5e3281b79f5e13a221
gilbruno <gilles_bruno@gmx.fr>
2025-01-15 16:23:07 +0100

    reset code

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-import { Address } from 'viem';
-
-import * as dotenv from "dotenv"
-dotenv.config()
-
-export const factoryAddress = process.env.ADDRESS_LAST_DEPLOYED_FACTORY as Address
-
-export const adminChallenge = process.env.ADMIN_CHALLENGE as Address
-export const adminDisputeChallenge = process.env.ADMIN_DISPUTE_CHALLENGE as Address
